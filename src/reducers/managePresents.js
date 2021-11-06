1090c969ff6eb56d31a2fdb45b5105eeec5249ea
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-06 13:44:50 -0500

    Revert "solved"
    This reverts commit d568f4550be6db18f921dbf1d34f0076e3f3ea3b.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,2 @@
 export function managePresents(state, action){
-    switch (action.type) {
-        case "INCREASE":
-            return {numberOfPresents: 1}
-        default:
-            return {numberOfPresents: 0}
-    }
 }
