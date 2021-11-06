833f689ff5169e7bd33e7b64671506ebc4438331
sergiocardenasg <sergiocardenas@utexas.edu>
2021-11-06 14:31:03 -0500

    solved

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return {friends: state.friends.concat(action.friend)}
+        case "REMOVE_FRIEND":
+            const remove = state.friends.findIndex(friend => friend.id === action.id);
+            return {friends: [...state.friends.slice(0, remove), ...state.friends.slice(remove + 1)]}
+        default:
+            return state
+    }
 }
